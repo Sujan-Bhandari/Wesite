240995164f5c5b8ee055c18d9b3ca46d8e8a6ba5
sujan bhandari <sujanbhandari170@gmail.com>
2024-06-03 15:28:09 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hellofkyjhg world!
-  </h1>
+    <>
+    <div className="bg-[green] p-6" >
+
+    <div className="max-w-[1240px] py-[15px] item-center flex justify-between  mx-auto">
+      <div className="text-3xl font-bold  text-yellow-300">
+        Info
+        <span className="text-black">dev</span> 
+      </div>
+      <AiOutlineMenu className="text-white text-5xl md:hidden block" />
+
+      <ul className="hidden md:flex text-red-300 gap-8">
+        <li>Home</li>
+        <li>Company</li>
+        <li>Resource</li>
+        <li>AboutUs</li>
+        <li>Contact</li>
+      </ul>
+      </div>  
+
+
+      </div>   
+    </>
   );
 }
 
