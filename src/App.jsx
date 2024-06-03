7efe154f56b8de378d62f3c6c19a678b715b86c8
sujan bhandari <sujanbhandari170@gmail.com>
2024-06-03 19:46:35 +0545

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
+
+
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+  
   return (
     <>
     <div className="bg-[green] p-6" >
@@ -11,8 +17,14 @@ function App() {
         Info
         <span className="text-black">dev</span> 
       </div>
-      <AiOutlineMenu className="text-white text-5xl md:hidden block" />
+      {
+        toggle ?
+        <AiOutlineClose onClick={()=>setToggle(!toggle)}  className="text-white text-5xl md:hidden block"/>
+        :
+        <AiOutlineMenu onClick={()=>setToggle(!toggle)} className="text-white text-5xl md:hidden block" />
 
+      }
+     
       <ul className="hidden md:flex text-red-300 gap-8">
         <li>Home</li>
         <li>Company</li>
@@ -20,6 +32,16 @@ function App() {
         <li>AboutUs</li>
         <li>Contact</li>
       </ul>
+
+      {/*Responsive menu */}
+      <ul className={" md:hidden w-full h-screen text-white  fixed bg-black  top-[126px] ${toggle ? 'left-[0]' :'left-[-100%]'"}>
+        <li className="p-5">Home</li>
+        <li className="p-5">Company</li>
+        <li className="p-5">Resource</li>
+        <li className="p-5">AboutUs</li>
+        <li className="p-5">Contact</li>
+      </ul>
+
       </div>  
 
 
